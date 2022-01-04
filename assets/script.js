f4a566a8baa7bd2d2811b210f2d70701bab7c296
Aldair <aldabino9@gmail.com>
2022-01-04 01:44:28 -0800

    podes

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+function vem(){
+    document.getElementById('form-container').style.transform = 'translate(0.1px)';
+    document.getElementById('form-container-master').style.display = 'flex'
+    document.getElementById('tutorial').style.width = '40%'
+}function vemc(){
+    document.getElementById('form-container2').style.transform = 'translate(1%)'
+    document.getElementById('form-container-master').style.display = 'flex'
+    document.getElementById('tutorial').style.width = '40%'
+}
+function vai(){
+    document.getElementById('form-container').style.transform = 'translate(290%)'
+    document.getElementById('form-container-master').style.display = 'none'
+    document.getElementById('tutorial').style.width = '100%'
+}
+
+function vaic(){
+    document.getElementById('form-container2').style.transform = 'translate(200%)'
+    document.getElementById('form-container-master').style.display = 'none'
+    document.getElementById('tutorial').style.width = '100%'
+
+}
+function vemm(){
+    document.getElementById('menu-lateral').style.transform = 'translateX(-0.01%)'
+    document.getElementById('barra-1').style.transform = 'rotate(46deg)'
+    document.getElementById('barra-1').style.marginTop = '51%'
+    document.getElementById('barra-1').style.marginBottom = '0'
+    document.getElementById('barra-2').style.transform = 'rotate(-46deg)'
+    document.getElementById('barra-2').style.marginTop = '0'
+    document.getElementById('barra-2').style.marginBottom = '51%'
+    document.getElementById('barra-3').style.display = 'none'
+    document.getElementById('botaov').style.display = 'inherit'
+}
+function vaime(){
+    console.log('uihs9h') 
+    document.getElementById('menu-lateral').style.transform = 'translateX(100%)'
+    document.getElementById('barra-3').style.display = 'block'
+    document.getElementById('barra-1').style.transform = 'rotate(0deg)'
+    document.getElementById('barra-1').style.marginTop = '20%'
+    document.getElementById('barra-1').style.marginBottom = '0'
+    document.getElementById('barra-2').style.transform = 'rotate(0deg)'
+    document.getElementById('barra-2').style.marginTop = '20%'
+    document.getElementById('barra-2').style.marginBottom = '0%'
+    document.getElementById('botaov').style.display = 'none'
+
+}
